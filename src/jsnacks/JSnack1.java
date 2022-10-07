15994fe3e1d28c51a6ec050dd10fe65078ea7817
MattiaVerardi <verardi.mattia97@gmail.com>
2022-10-07 15:31:43 +0200

    consegna JSnack1 13/06/22

---
+++ src/jsnacks/JSnack1.java
@@ -0,0 +1,28 @@
+package jsnacks;
+
+import java.util.Scanner;
+
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+//Esegui questo programma in due versioni, con il for e con il while.
+
+public class JSnack1 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int numero = 0;
+		int somma = 0;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Inserisci un numero");
+			numero = scan.nextInt();
+			somma = somma + numero;
+		}
+
+		System.out.println(somma);
+
+		scan.close();
+	}
+}
